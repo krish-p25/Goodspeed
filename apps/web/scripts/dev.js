46eae6e3dae4b44f09438774104ef3d410d33ee3
Krish P <80683788+krish-p25@users.noreply.github.com>
2026-06-19 18:32:26 +0100

    Added build info and decreased compile time

--- apps/web/scripts/dev.js
+++ apps/web/scripts/dev.js
@@ -1,3 +1,3 @@
 const { spawnSync } = require('child_process');
 const port = process.env.WEB_PORT || '3020';
-spawnSync('next', ['dev', '--port', port], { stdio: 'inherit', shell: true });
+spawnSync('next', ['dev', '--turbo', '--port', port], { stdio: 'inherit', shell: true });
