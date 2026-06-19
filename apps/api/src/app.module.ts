45069a13ec9f0f9505df5e1b29cfa959661354f3
Krish P <80683788+krish-p25@users.noreply.github.com>
2026-06-19 17:56:46 +0100

    Connected documents module to API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { SupabaseModule } from './supabase/supabase.module'
 import { AuthModule } from './auth/auth.module'
+import { DocumentsModule } from './documents/documents.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '../../.env' }),
     SupabaseModule,
     AuthModule,
+    DocumentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
